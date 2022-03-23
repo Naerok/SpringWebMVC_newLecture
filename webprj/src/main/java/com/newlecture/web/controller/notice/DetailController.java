98720455495bdeffc20485d223c993e2345d2af7
naerok <chg9252@gmail.com>
2022-03-24 00:52:13 +0900

    add noticeController, add detailController, buildCommonViewPage, build CommonHeaderPage, separate pageModule

--- webprj/src/main/java/com/newlecture/web/controller/notice/DetailController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/DetailController.java
@@ -6,14 +6,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class ListController implements Controller{
+public class DetailController implements Controller{
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		ModelAndView mv = new ModelAndView("notice/list");
+		ModelAndView mv = new ModelAndView("notice/detail");
 		
-		//mv.setViewName("/WEB-INF/view/notice/list.jsp");
+		//mv.setViewName("/WEB-INF/view/notice/detail.jsp");
+		
 		return mv;
 	}
 
